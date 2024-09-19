34e604321b963bcdafe472955df06134cb5d7d8d
shubham-ajabe <shubhamajabe2001@gmail.com>
2024-09-19 17:28:21 +0530

    Inital commit

---
+++ accounts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
